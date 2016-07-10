58536b2d65d1129352a3ee504295895584a802da
Bryan Brewster <bryan.brewster@linuxmail.org>
2016-07-10 02:49:07 -0600

    Initial commit.

---
+++ SongThatNeverEnds.bat
@@ -0,0 +1,65 @@
+@echo off
+:start
+
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo                          This is the song that never ends
+files\wav files\line1.wav /q
+::ping 127.0.0.1 >nul
+
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo                          It just goes on and on my friends
+::ping 127.0.0.1 >nul
+files\wav files\line2.wav /q
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo           Some people started singing it not knowing what it was
+::ping 127.0.0.1 >nul
+files\wav files\line3.wav /q
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo                and they continued singing it forever just because
+::ping 127.0.0.1 >nul
+files\wav files\line4.wav /q
+goto start
